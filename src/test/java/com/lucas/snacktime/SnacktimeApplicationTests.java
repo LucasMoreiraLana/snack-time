3148d64b3263e816221aa76d51aba932a5d4f264
Lucas Moreira <lucasmoreiralana04@gmail.com>
2025-04-09 09:27:07 -0300

    Primeiro commit Snack Time

---
+++ src/test/java/com/lucas/snacktime/SnacktimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.snacktime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnacktimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
